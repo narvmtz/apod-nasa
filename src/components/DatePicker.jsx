005e2fd1df1e1829035df81bae5cc92569a6bc11
nar <51009725+narvmtz@users.noreply.github.com>
2022-01-19 23:07:15 -0500

    Add subtitle description

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -3,18 +3,24 @@ import './fetchData.css';
 
 const DatePicker = ({ changeHandler, dateFormat, futureDate }) => {
   return (
-    <form className="date-picker">
-      <label>
-        Select date:
-        <input
-          type="date"
-          max={format(new Date(), dateFormat)}
-          name="futureDate"
-          value={futureDate}
-          onChange={(e) => changeHandler(e)}
-        />
-      </label>
-    </form>
+    <>
+      <p style={{ padding: '0 1rem' }}>
+        Hey space-lover 👋! Check out the previous 7 pictures (with
+        descriptions!) from APOD NASA's API by selecting a date!
+      </p>
+      <form className="date-picker">
+        <label>
+          Select date:
+          <input
+            type="date"
+            max={format(new Date(), dateFormat)}
+            name="futureDate"
+            value={futureDate}
+            onChange={(e) => changeHandler(e)}
+          />
+        </label>
+      </form>
+    </>
   );
 };
 
